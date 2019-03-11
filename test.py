30538136e97ef9f44674ddd2c1b1ca8b17e91abc
philcl <44048815+philcl@users.noreply.github.com>
2019-03-11 14:37:36 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -10,40 +10,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
+def calculBarycentre(X, Y, Z, ZY) :
+    w1 = []
+    w2 = []
+    m1 = []
+    TEST = []
+    
+    Zdist = np.zeros((10, len(Z)))
+    
+    for cpt_class in range(0, 10):
+        w1.append(X[Y == cpt_class]) #créer le tableau X avec le bon nombre de cases
+        m1.append(np.mean(w1[cpt_class], axis=0)) # Création du tableau des barycentres des classes (les points sont des images)
+        w2.append(Z[ZY == cpt_class]) #créer le tableau Z avec le bon nombre de cases
+    
+    for i in range(0, 10) :
+        Zdist[i] = (abs((m1[i] - Z))**2).sum(axis=1)
+    
+    ZClasse = np.argmin(Zdist, axis=0)
+    
+    TEST.append(ZClasse == ZY)
+    
+    performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
+    print(performance*100)
+
+
+def calculSVM(X, Y, Z, ZY) :
+    print("hello")
+    
+
+
 X = np.load('data/trn_img.npy')
 Y = np.load('data/trn_lbl.npy')
 Z = np.load('data/dev_img.npy')
 ZY = np.load('data/dev_lbl.npy')
 
-img = X[0].reshape(28,28)
+img = X[1].reshape(28,28)
 plt.imshow(img, plt.cm.gray)
 plt.show()
 
+calculBarycentre(X, Y, Z, ZY)
 
-w1 = []
-w2 = []
-m1 = []
-TEST = []
+Xpca = PCA(n_components = 30)
+Xpca.fit(X)                 # Créer la matrice de passage pour chznger de dimmension entre 784 et 30 pour cet exemple.
+XReduit = Xpca.transform(X) #Reduction du nombre de dimmension de X
 
-Zdist = np.zeros((10, len(Z)))
+#print(Xpca.components_)
 
-for cpt_class in range(0, 10):
-    w1.append(X[Y == cpt_class]) #créer le tableau X avec le bon nombre de cases
-    m1.append(np.mean(w1[cpt_class], axis=0)) # Création du tableau des barycentres des classes (les points sont des images)
-    w2.append(Z[ZY == cpt_class]) #créer le tableau X avec le bon nombre de cases
-       
-cpt = 0 
-saClasse = 0
-
-for i in range(0, 10) :
-    Zdist[i] = (abs((m1[i] - Z))**2).sum(axis=1)
-
-ZClasse = np.argmin(Zdist, axis=0)
-
-TEST.append(ZClasse == ZY)
-
-performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
-print(performance*100)
-
-Xpca = PCA(n_components = 2)
-Xpca.fit(X)
+ZReduit = Xpca.transform(Z)
+calculBarycentre(XReduit, Y, ZReduit, ZY)
