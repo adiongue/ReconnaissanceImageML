abe85262841d07da9d78061d205d58a45e56f100
philcl <44048815+philcl@users.noreply.github.com>
2019-03-11 16:34:27 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ Created on Wed Feb 13 08:58:59 2019
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn import svm
 
 def calculBarycentre(X, Y, Z, ZY) :
     w1 = []
@@ -34,10 +35,84 @@ def calculBarycentre(X, Y, Z, ZY) :
     print(performance*100)
 
 
-def calculSVM(X, Y, Z, ZY) :
-    print("hello")
+def calculSVMLin(X, Y, Z, ZY) :
+    print("Calcul SVM Linear")
+    TEST = []
+    TEST2 = []
     
+    lin = svm.LinearSVC(max_iter=2100)
+    lin.fit(X, Y)
+    Zpredict = lin.predict(Z)
+    Xpredict = lin.predict(X)
+    TEST.append(Zpredict == ZY)
+    TEST2.append(Xpredict == Y)
+    
+    print("Performance Dev")
+    performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
+    print(performance*100)
+    
+    print("Performance Train")
+    performance = (len(sum(TEST2)) - sum(TEST2).sum())/len(sum(TEST2))
+    print(performance*100)
+    
+def calculSVMSvr(X, Y, Z, ZY) :
+    print("Calcul SVM SVR")
+    TEST = []
+    TEST2 = []
+    svr = svm.SVC(kernel='linear', max_iter=2100)
+    svr.fit(X, Y)
+    Zpredict = svr.predict(Z)
+    Xpredict = svr.predict(X)
+    TEST.append(Zpredict == ZY)
+    TEST2.append(Xpredict == Y)
+    
+    print("Performance Dev")
+    performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
+    print(performance*100)
+    
+    print("Performance Train")
+    performance = (len(sum(TEST2)) - sum(TEST2).sum())/len(sum(TEST2))
+    print(performance*100)
 
+
+def calculSVMRbf(X, Y, Z, ZY) :
+    print("Calcul SVM RBF")
+    TEST = []
+    TEST2 = []
+    svr = svm.SVC(max_iter=2100)
+    svr.fit(X, Y)
+    Zpredict = svr.predict(Z)
+    Xpredict = svr.predict(X)
+    TEST.append(Zpredict == ZY)
+    TEST2.append(Xpredict == Y)
+    
+    print("Performance Dev")
+    performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
+    print(performance*100)
+    
+    print("Performance Train")
+    performance = (len(sum(TEST2)) - sum(TEST2).sum())/len(sum(TEST2))
+    print(performance*100)
+
+
+def calculSVMPoly(X, Y, Z, ZY) :
+    print("Calcul SVM Poly")
+    TEST = []
+    TEST2 = []
+    svr = svm.SVC(kernel='poly', max_iter=2100)
+    svr.fit(X, Y)
+    Zpredict = svr.predict(Z)
+    Xpredict = svr.predict(X)
+    TEST.append(Zpredict == ZY)
+    TEST2.append(Xpredict == Y)
+    
+    print("Performance Dev")
+    performance = (len(sum(TEST)) - sum(TEST).sum())/len(sum(TEST))
+    print(performance*100)
+    
+    print("Performance Train")
+    performance = (len(sum(TEST2)) - sum(TEST2).sum())/len(sum(TEST2))
+    print(performance*100)
 
 X = np.load('data/trn_img.npy')
 Y = np.load('data/trn_lbl.npy')
@@ -48,13 +123,36 @@ img = X[1].reshape(28,28)
 plt.imshow(img, plt.cm.gray)
 plt.show()
 
-calculBarycentre(X, Y, Z, ZY)
-
 Xpca = PCA(n_components = 30)
 Xpca.fit(X)                 # Créer la matrice de passage pour chznger de dimmension entre 784 et 30 pour cet exemple.
 XReduit = Xpca.transform(X) #Reduction du nombre de dimmension de X
 
 #print(Xpca.components_)
-
 ZReduit = Xpca.transform(Z)
+
+print("sans PCA")
+calculBarycentre(X, Y, Z, ZY)
+print("avec PCA")
 calculBarycentre(XReduit, Y, ZReduit, ZY)
+
+print("sans PCA")
+calculSVMLin(X[:2000], Y[:2000], Z[:2000], ZY[:2000])
+print("avec PCA")
+calculSVMLin(XReduit[:2000], Y[:2000], ZReduit[:2000], ZY[:2000])
+
+print("sans PCA")
+calculSVMSvr(X[:2000], Y[:2000], Z[:2000], ZY[:2000])
+print("avec PCA")
+calculSVMSvr(XReduit[:2000], Y[:2000], ZReduit[:2000], ZY[:2000])
+
+print("sans PCA")
+calculSVMRbf(X[:2000], Y[:2000], Z[:2000], ZY[:2000])
+print("avec PCA")
+calculSVMRbf(XReduit[:2000], Y[:2000], ZReduit[:2000], ZY[:2000])
+
+
+print("sans PCA")
+calculSVMPoly(X[:2000], Y[:2000], Z[:2000], ZY[:2000])
+print("avec PCA")
+calculSVMPoly(XReduit[:2000], Y[:2000], ZReduit[:2000], ZY[:2000])
+#svc(X, Y)
